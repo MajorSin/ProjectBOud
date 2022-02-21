e0a8d926c33647876233b7cc645e26070040624d
MajorSin <pssanghera0@gmail.com>
2022-02-21 21:45:36 +0100

    Film overzicht gemaakt

---
+++ Film overzicht/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyApp {
+    public class Program {
+        public class filmOverzicht {
+            public string? Title { get; set; }
+            public int year { get; set; }
+        }
+        static void Main() {
+            //INTRO BERICHT
+            Console.WriteLine("----------------------------\nBIOSCOOP HR - FILMS OVERZICHT\n----------------------------\nHier bevindt een overzicht van alle films, je kunt de resultaten filteren om een duidelijk overzicht te krijgen.\n");
+            //LAAT FILM OVERZICHT ZIEN
+            string json = System.IO.File.ReadAllText("../../../films.json");
+            var films = Newtonsoft.Json.JsonConvert.DeserializeObject<List<filmOverzicht>>(json);
+            foreach (var film in films) {
+                Console.WriteLine($"- {film.Title} ({film.year})\n   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus quis dolor cursus porta. Nulla et arcu suscipit, blandit lectus a, pellentesque odio. Nunc suscipit est a lacus faucibus, ut vehicula metus efficitur. Integer nec convallis nisi. Aenean justo diam, blandit quis porttitor vitae, tincidunt vel ex. \n");
+            }
+        }
+    }
+}
