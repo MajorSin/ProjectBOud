9d9f6dc81e71d79c9c951412ff6bc087c5232efb
MajorSin <pssanghera0@gmail.com>
2022-02-23 18:49:05 +0100

    Film details toegevoegd en Excel sheet met alle film informatie

--- Film overzicht/Program.cs
+++ Film overzicht/Program.cs
@@ -4,17 +4,76 @@ using Newtonsoft.Json.Linq;
 namespace MyApp {
     public class Program {
         public class filmOverzicht {
-            public string? Title { get; set; }
-            public int year { get; set; }
+            public int? Id { get; set; }
+            public string? Titel { get; set; }
+            public int? Jaar { get; set; }
+            public List<string>? Taal { get; set; }
+            public int? Looptijd { get; set; }
+            public List<string>? Genre { get; set; }
+            public string? Directeur { get; set; }
+            public string? Acteurs { get; set; }
+            public string? Plot { get; set; }
+
         }
         static void Main() {
             //INTRO BERICHT
-            Console.WriteLine("----------------------------\nBIOSCOOP HR - FILMS OVERZICHT\n----------------------------\nHier bevindt een overzicht van alle films, je kunt de resultaten filteren om een duidelijk overzicht te krijgen.\n");
+            Console.WriteLine("----------------------------\nBIOSCOOP HR - FILMS OVERZICHT\n----------------------------\nHier kunt u een overzicht van alle films vinden.\n");
             //LAAT FILM OVERZICHT ZIEN
             string json = System.IO.File.ReadAllText("../../../films.json");
-            var films = Newtonsoft.Json.JsonConvert.DeserializeObject<List<filmOverzicht>>(json);
-            foreach (var film in films) {
-                Console.WriteLine($"- {film.Title} ({film.year})\n   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus quis dolor cursus porta. Nulla et arcu suscipit, blandit lectus a, pellentesque odio. Nunc suscipit est a lacus faucibus, ut vehicula metus efficitur. Integer nec convallis nisi. Aenean justo diam, blandit quis porttitor vitae, tincidunt vel. \n");
+            var films = JsonConvert.DeserializeObject<List<filmOverzicht>>(json);
+            if (films != null) {
+                foreach (var film in films) {
+                    //KRIJG DE TALEN
+                    int taalLength = film?.Taal?.Count ?? 0;
+                    string talen = "";
+                    for (int i = 0; i < taalLength; i++) {
+                        talen += film?.Taal?[i];
+                        //VOEG DE JUISTE TEKEN TOE
+                        if (taalLength > 1) {
+                            if ((taalLength - 2) == i) {
+                                talen += " & ";
+                            } else if ((taalLength - 1) != i) {
+                                talen += ", ";
+                            }
+                        }
+                    }
+                    string taalString = taalLength == 1 ? "Taal" : "Talen";
+                    //KRIJG DE GENRE(S)
+                    int genreLength = film?.Genre?.Count ?? 0;
+                    string genres = "";
+                    for (int i = 0; i < genreLength; i++) {
+                        genres += film?.Genre?[i];
+                        //VOEG DE JUISTE TEKEN TOE
+                        if (genreLength > 1) {
+                            if ((genreLength - 2) == i) {
+                                genres += " & ";
+                            } else if ((genreLength - 1) != i) {
+                                genres += ", ";
+                            }
+                        }
+                    }
+                    string genreString = genreLength == 1 ? "Genre" : "Genres";
+                    //LAAT ALLES BOVEN DE 1 UUR ZIEN
+                    if (film?.Looptijd > 60) {
+                        int? uur = film.Looptijd / 60;
+                        int? minuten = film.Looptijd - (uur * 60);
+                        string uurString = uur == 1 ? "uur" : "uren";
+                        string minuutString = minuten == 1 ? "minuut" : "minuten";
+                        //PRINT DE GEGEVENS
+                        Console.WriteLine($"- {film.Titel} ({film.Jaar}) \n" +
+                            $"  - {taalString} : {talen} \n" +
+                            $"  - Looptijd: {uur} {uurString} en {minuten} {minuutString}.\n" +
+                            $"  - {genreString} : {genres}\n" +
+                            $"  - Directeur(s) : {film.Directeur}\n" +
+                            $"  - Acteurs : {film.Acteurs}\n" + 
+                            $"  - Plot: {film.Plot}\n");
+                    }
+                    //LAAT ALLES ONDER DE 1 UUR ZIEN
+                    else {
+                        //PRINT DE GEGEVENS
+                        Console.WriteLine($"- {film?.Titel} ({film?.Jaar}), \nPlot: {film?.Plot}\nLooptijd: {film?.Looptijd} minuten \n");
+                    }
+                }
             }
         }
     }
